f293cfd534a4656c0d09d808de19567ec1749823
Stefan Körner <contact@skoerner.com>
2020-10-11 15:02:24 +0200

    123

--- sitechecker/forms.py
+++ sitechecker/forms.py
@@ -1,7 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-
-
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ("email",)
+
+    class Meta:
+        fields = ('username','email','password1','password2')
+        model = get_user_model()
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['email'].required = True
